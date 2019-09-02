3ca9ac63ba5c7a37694145ffc1d88ff6d166af52
Ben <bigriffin94@outlook.com>
2019-09-02 12:13:12 -0700

    Project Complete.

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,24 @@
+/* 
+<TodoForm> will hold your input field and your Add Todo and Clear Completed buttons.
+Your input field should take in user input, and allow a user to press Enter or click on the Submit Button to add a todo to your list.
+Once a todo is submitted, the Todo List should re-render and show the added todo.
+*/
+    
+import React from 'react';
+const TodoForm = props => {
+  return (
+    <form>
+      <input
+        onChange={props.handleTodoChange}
+        type="text"
+        name="todo"
+        value={props.value}
+        placeholder="...todo"
+      />
+      <button onClick={props.handleAddTodo}>Add Todo</button>
+      <button onClick={props.handleClearTodos}>Clear Completed</button>
+    </form>
+  );
+};
+
+export default TodoForm;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,27 @@
+import React from 'react';
 // your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// display a list of todos
+//an input field
+// a submit button
+//and a clear all button.
+// <TodoList /> receives your Todos array and iterates over the list generating a new <Todo /> for each element in the array. 
+
+import Todo from './Todo';
+
+const TodoList = props => {
+  return (
+    <div>
+      {props.todos.map(todo => (
+        <Todo
+          handleToggleComplete={props.handleToggleComplete}
+          key={todo.id}
+          todo={todo}
+        />
+      ))}
+        {console.log(props.todos)}
+    </div>
+  );
+};
+
+export default TodoList;
+
